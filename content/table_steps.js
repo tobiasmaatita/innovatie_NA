28b484800980d6c2b45f33af5bd5f2fc6cc29c21
tobiasmaatita <tobiasmaatita@outlook.com>
2023-11-19 11:50:33 +0100

    first steps table

--- content/table_steps.js
+++ content/table_steps.js
@@ -2,11 +2,35 @@ table = d3.select('body').append('table').style("border-collapse", "collapse").s
 
 const rows = [
     ['a', 'b', 'c'],
-    [1, 2, 3]
+    [1, 2, 3],
+    [4, 5, 6]
 ];
 
 table.append("thead").append("tr")
-.select_all("th")
+.selectAll("th")
 .data(rows[0])
 .enter().append("th")
 .text(function(d) {return d; })
+.style("border", "1px black solid")
+.style("padding", "5px")
+.style("background-color", "lightgray")
+.style("font-weight", "bold")
+.style("text-transform", "uppercase");
+
+table.append("tbody")
+.selectAll("tr").data(rows.slice(1))
+.enter().append("tr")
+.selectAll("td")
+.data(function(d){return d; })
+.enter().append("td")
+.style("border", "1px black solid")
+.style("padding", "5px")
+.on("mouseover", function(){
+    d3.select(this).style("background-color", "powderblue");
+})
+.on("mouseout", function(){
+    d3.select(this).style("background-color", "white");
+})
+.text(function(d){return d; })
+.style("font-size", "12px");
+
