01159d9ddede31036191909aa17064ecec66f52c
tobiasmaatita <tobiasmaatita@outlook.com>
2023-11-20 14:36:42 +0100

    MVP

--- content/table_steps.js
+++ content/table_steps.js
@@ -1,47 +1,72 @@
-// d3.text("tester_tabel.tsv").then(function(datasetText){
-//     var rows = d3.tsvParseRows(datasetText)
-//     var table = d3.select('body')
-//     .append('table')
-//     .style("border-collapse", "collapse")
-//     .style("border", "2px black solid");
+d3.selectAll(".text").on("mouseover", function() {
+    d3.selectAll(".text").style("opacity", 0.6);
+    d3.selectAll(".subClass").style("opacity", 1)
+    d3.select(this).style("opacity", 1);
+});
 
-// table.append("thead").append("tr")
-// .selectAll("th")
-// .data(rows[0])
-// .enter().append("th")
-// .text(function(d) {return d; })
-// .style("border", "1px black solid")
-// .style("padding", "5px")
-// .style("background-color", "lightgray")
-// .style("font-weight", "bold")
-// .style("text-transform", "uppercase");
+d3.selectAll(".text").on("mouseout", function() {
+    d3.selectAll(".text").style("opacity", 1);
+});
 
-// table.append("tbody")
-// .selectAll("tr").data(rows.slice(1))
-// .enter().append("tr")
-// .selectAll("td")
-// .data(function(d){return d; })
-// .enter().append("td")
-// .style("border", "1px black solid")
-// .style("padding", "5px")
-// .style("background-color", "powderblue")
-// .on("mouseover", function(){
-//     d3.select(this).style("background-color", "darkblue")
-//     .style("color", "white");
+// d3.selectAll("#governance").on("click", function(){
+//     console.log("klik")
+//     d3.selectAll(".tableStrategie").transition().duration(1000).attr("hidden", true);
 // })
-// .on("mouseout", function(){
-//     d3.select(this).style("background-color", "powderblue")
-//     .style("color", "black");
+
+// d3.selectAll("#ecosysteem").on("click", function(){
+//     d3.selectAll(".tableStrategie").transition().duration(1000).attr("hidden", null)
 // })
-// .text(function(d){return d; })
-// .style("font-size", "12px");
+var wrapper = d3.select("#bigWrapper");
+var menuWrapper = wrapper.append("div").attr("id", "menuWrapper");
+var ul = menuWrapper.append("ul").attr("id", "menu");
+let hidden = {
+    ".tableStrategie": false, 
+    ".tableResultaat": false, 
+    ".tableGovernance": false, 
+    ".tableEcosysteem": false, 
+    ".tableCultuur": false
+};
+const sumValues = obj => Object.values(obj).reduce((a, b) => a + b, 0);
+var errorMessageGiven = false
 
-// });
+d3.select("#menu")
+.selectAll("li")
+.data(["Strategie", "Ecosysteem", "Cultuur", "Governance", "Resultaat"])
+.enter()
+.append("li")
+.text(function(d) {return d; })
+.classed("selected", function(d){
+    return hidden[`.table${d}`];
+})
+.on("click", function(d){
+    if (sumValues(hidden) == 4) {
+        if (hidden[`.table${d}`] == false) {
+            errorText = d3.select("body").append("div").attr("id", "errorMessage");
+            errorMessageGiven = true;
+            errorText.append("p").text("Je kan niet alle categorieën wegklikken.\n \
+            Kies een andere categorie om door te gaan of klik hier om deze waarschuwing te verwijderen.")
+            .on("click", function() {
+                d3.selectAll("#errorMessage").remove();
+                errorMessageGiven = false;
+            });
+            return;
+        };
+    };
+    var hide = (hidden[`.table${d}`] == false) ? true : null;
+    (hide == true) ? hidden[`.table${d}`] = true : hidden[`.table${d}`] = false;
+    d3.selectAll(`.table${d}`).attr("hidden", hide);
+    updateList(d);
+    if (errorMessageGiven) {
+        d3.selectAll("#errorMessage").remove();
+        errorMessageGiven = false;
+    }
+    console.log(sumValues(hidden))
+});
 
-var table = d3.select("body").select("table")
+function updateList(typeInfo) {
+    // update menu with selected info
+    d3.select("#menu").selectAll("li").classed("selected", function(d) {
+        return d == typeInfo;
+    });
+};
 
-function mouseOver(d) {
-    d3.select(this).append("text").text(function(){return "HALLO"})
-}
-
-table.selectAll("th").on("mouseover", mouseOver)
